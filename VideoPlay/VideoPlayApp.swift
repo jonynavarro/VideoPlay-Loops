2ec54433d711bb6e67cd561d6fe521cef39cc9db
iony-dark87 <nathandeveloper87@gmail.com>
2023-10-21 09:46:55 -0600

    Initial Commit

---
+++ VideoPlay/VideoPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayApp.swift
+//  VideoPlay
+//
+//  Created by Jony Navarro on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
